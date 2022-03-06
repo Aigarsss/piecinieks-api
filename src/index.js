8df19f3de1b35445b39534cd5476a705d26411de
Aigars U <aigars.uplejs@gmail.com>
2022-03-06 15:56:47 +0200

    Add helmet, cors and graphql validation rules

--- src/index.js
+++ src/index.js
@@ -5,12 +5,18 @@ const jwt = require('jsonwebtoken');
 const db = require('./db');
 const models = require('./models');
 const typeDefs = require('./schema');
-const resolvers = require('./resolvers')
+const resolvers = require('./resolvers');
+const helmet = require('helmet');
+const cors = require('cors');
+const depthLimit = require('graphql-depth-limit');
+const { createComplexityLimitRule } = require('graphql-validation-complexity');
 
 const PORT = process.env.PORT || 4000;
 const DB_HOST = process.env.DB_HOST;
 
 const app = express();
+app.use(helmet());
+app.use(cors());
 db.connect(DB_HOST);
 
 const getUser = (token) => {
@@ -26,6 +32,7 @@ const getUser = (token) => {
 const server = new ApolloServer({
     typeDefs,
     resolvers,
+    validationRules: [depthLimit(5), createComplexityLimitRule(1000)],
     context: ({ req }) => {
         const token = req.headers.authorization;
         const user = getUser(token);
